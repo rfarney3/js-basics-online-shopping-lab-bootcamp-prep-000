289d0bddee22d1362c3ced134ab6f45793e6638f
Ryan Farney <farneyr@yahoo.com>
2017-12-13 20:04:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,14 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+  var str = '';
+  if (cart.length === 0) {
+    return 'Your shopping cart is empty.';
+  }
+  
+  for (var i = 0; i < cart.length; i++) {
+    
+  }
 }
 
 function total() {
